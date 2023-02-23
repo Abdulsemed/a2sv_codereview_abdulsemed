66e72fb49ff1fa5d0cb4e26b8063dc7ca9c1ba3f
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-02-23 16:13:10 +0300

    Update 3-longest repeating character replacement.py

--- 3-longest repeating character replacement.py
+++ 3-longest repeating character replacement.py
@@ -1,28 +1,15 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        sDict = {}
         size = len(s)
-        if size < 2:
-            return 1
         maxLen = 0
-        left = 0 
-        right = 1
-        leftCount = 0
-        rightCount = 0
-        leftRightCount =0
         while right < size:
-            if s[right] != s[left]:
-                rightCount += 1
-                if rightCount == k:
-                    maxLen = max(maxLen,leftRightCount+rightCount+leftCount)
-                    left += 1
-                    leftCount = rightCount
-                    rightCount = leftRightCount 
-                    leftRightCount = 0
-            else:
-                if rightCount != leftRightCount:
-                    leftCount += 1
-                else:
-                    leftRightCount += 1
-                right += 1
-                
-        
+            sDict[s[right]] = 1 + sDict.get(s[right],0)
+            while sDict[s[right]] > 1:
+                sDict[s[left]] -= 1
+                left += 1
+            maxLen = max(maxLen, right -left +1)
+            right += 1
+        return maxLen
